0b3b1f4a8d41424a6de62e220d19f5bbd058d5c4
Alex Morega <alex@grep.ro>
2013-02-14 18:52:06 +0200

    deploy jenkins app

--- fabfile.py
+++ fabfile.py
@@ -1,10 +1,23 @@
 import os
+import subprocess
+from StringIO import StringIO
 from fabric.api import *
 
 JENKINS_WAR = 'http://mirrors.jenkins-ci.org/war/latest/jenkins.war'
 _host, TARGET_DIRECTORY = os.environ['DEPLOYMENT_TARGET'].split(':')
 env['hosts'] = [_host]
 env['use_ssh_config'] = True
+
+
+@task
+def deploy():
+    tarball = subprocess.check_output(['git', 'archive', 'HEAD'])
+    with cd(TARGET_DIRECTORY):
+        put(StringIO(tarball), '_app.tar')
+        try:
+            run('bin/sarge deploy _app.tar web')
+        finally:
+            run('rm _app.tar')
 
 
 @task
